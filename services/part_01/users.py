8c198503a717927fa3edc96807056c4cae6b71c3
tklepacki <t.klepacki@wp.pl>
2025-10-12 17:34:33 +0200

    Update users.py

--- services/part_01/users.py
+++ services/part_01/users.py
@@ -1,10 +1,13 @@
 import requests
 
+API_KEY = "reqres-free-v1"
+HEADERS = {"x-api-key": API_KEY}
+
 def get_user(id):
     url = f"https://reqres.in/api/users/{id}"
 
     session = requests.session()
-    response = session.get(url)
+    response = session.get(url, headers=HEADERS)
 
     return response
 
@@ -12,6 +15,6 @@ def get_users_list(page_id):
     url = "https://reqres.in/api/users"
 
     session = requests.session()
-    response = session.get(url, params={'page': page_id})
+    response = session.get(url, params={'page': page_id}, headers=HEADERS)
 
     return response
